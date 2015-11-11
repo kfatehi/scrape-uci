81f60a8d7871dba01eac8926c7117316ea6f88c6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-11-11 12:25:01 -0800

    ability to scrape all departments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var scrapeUCI = require('./lib/uci/scrape');
-var args = require('minimist')(process.argv)
 
-var period = args.p || args.period
-var year = args.y || args.year
-var dept = args.d || args.dept
-
-scrapeUCI(year, period, 'IN4MATX', function(err, courses) {
-  if (err) throw err;
-  else console.log(courses);
-});
+if (module.parent) {
+  module.exports = scrapeUCI
+} else {
+  var args = require('minimist')(process.argv)
+  scrapeUCI({
+    year: args.y || args.year,
+    period: args.p || args.period,
+    dept: args.d || args.dept
+  }, function(err, courses) {
+    if (err) throw err;
+    else console.log(courses);
+  });
+}
--- test/lib/uci/scrape-test.js
+++ test/lib/uci/scrape-test.js
@@ -3,15 +3,32 @@ var expect = require('chai').expect
 var scrapeUCI = require('../../../lib/uci/scrape')
 
 describe('uci scraper', function() {
-  it('works', function(done) {
+  it('gets courses of a single department if one is specified', function(done) {
     this.timeout(5000);
-    scrapeUCI('2016', 'Winter', 'IN4MATX', function(err, data) {
+    scrapeUCI({
+      year:'2016',
+      period:'Winter',
+      dept:'IN4MATX'
+    }, function(err, courses) {
       if (err) return done(err);
       expect(err).to.be.a('null');
-      var course = data[0];
+      expect(courses).to.have.length(22);
+      var course = courses[0];
       expect(course.id).to.eq('In4matx113')
-      console.log(course);
       expect(course.prerequisite).to.match(/IN4MATX\s+43/)
+      done();
+    })
+  });
+
+  it('gets courses of all departments if none specified', function(done) {
+    this.timeout(10000);
+    scrapeUCI({
+      year:'2016',
+      period:'Winter',
+    }, function(err, courses) {
+      if (err) return done(err);
+      expect(err).to.be.a('null');
+      expect(courses).to.have.length(3299);
       done();
     })
   });
