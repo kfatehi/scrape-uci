cfc3d3354b7aaa055b3855d0d08bd376e0217166
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-11-11 14:56:12 -0800

    add another prereq example

--- test/lib/uci/prereq-parser-test.js
+++ test/lib/uci/prereq-parser-test.js
@@ -3,8 +3,8 @@ var _ = require('lodash');
 var courses = require('../../../fixtures/2016-winter-raw-prereq');
 var pp = require('../../../lib/uci/prereq-parser');
 
-describe('prereq parser', function() {
-  it.only("parses reqs for IN4MATX 113", function() {
+describe.only('prereq parser', function() {
+  it("parses reqs for IN4MATX 113", function() {
     var course = _.find(courses, { id: 'In4matx113' })
     var reqs = pp.parse(course.prerequisite)
     expect(reqs).to.deep.eq({
@@ -34,4 +34,33 @@ describe('prereq parser', function() {
       }]
     })
   })
+
+  it('parses reqs for In4matx122', function() {
+    var course = _.find(courses, { id: 'In4matx122' })
+    var reqs = pp.parse(course.prerequisite)
+    expect(reqs).to.deep.eq({
+      "type": "exactly",
+      "aggregateBy": null,
+      "select": 2,
+      "elements": [{
+        "type": "exactly",
+        "aggregateBy": null,
+        "elements": [
+          "I&C SCI 45J",
+          "I&C SCI 46",
+          "IN4MATX 45"
+        ],
+        "select": 1
+      },{
+        "type": "exactly",
+        "aggregateBy": null,
+        "elements": [
+          "IN4MATX 101",
+          "COMPSCI 141",
+          "CSE 141"
+        ],
+        "select": 1
+      }]
+    })
+  });
 })
